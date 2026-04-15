7248e02baac691f8148b78bab40cbe295433c845
Gabriel Alves <g4briel.alves9@gmail.com>
2026-04-15 11:44:49 -0300

    Update index.js to use import/export syntax instead of require

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 
 app.use(express.json());
 
-const funcionarioRoutes = require("./src/routes/funcionarioRoutes");
+const funcionarioRoutes = (await import("./src/routes/funcionarioRoutes.js")).default;
 app.use("/funcionarios", funcionarioRoutes);
 
 app.listen(3000, () => console.log("API rodando"));
