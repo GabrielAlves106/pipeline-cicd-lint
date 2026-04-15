3df6710287557adccbd66f7ef352d0804b7bd598
Gabriel Alves <g4briel.alves9@gmail.com>
2026-04-15 11:45:46 -0300

    Updated eslint.config.mjs with new ESLint configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: {...globals.node, ...globals.jest }},
-]);
+export default [
+  {
+    ignores: ["node_modules/", "dist/", "coverage/", ".github/", "build/", "package-lock.json"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: js.configs.recommended.rules
+  }
+];
